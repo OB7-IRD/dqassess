40eeef1ac98ff7a7db6d61f4cabc060ead6c2fa7
Mathieu Depetris <mathieu.depetris@ird.fr>
2019-04-04 15:19:08 +0200

    Modifications

---
+++ vignettes/dqassess_controls.R
@@ -0,0 +1,16 @@
+## ----loading_library, eval = FALSE, include = TRUE-----------------------
+#  # Devtools is a necessary package
+#  # If is not installed, run the following line
+#  install.packages("devtools")
+#  # Load the package from the Git
+#  devtools::install_git("https://github.com/OB7-IRD/dqassess.git")
+#  # Load the library
+#  library(dqassess)
+#  # You can access to the package documentation with the following line
+#  ?dqassess
+#  # If you want the documentation of a specific package function use the same syntax, for example for the function build_template_format_db
+#  ?build_template_format_db
+
+## ----checking_data, eval = FALSE, include = TRUE-------------------------
+#  
+
